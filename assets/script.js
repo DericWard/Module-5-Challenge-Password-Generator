06c612eab30c4a36ce4cc1d9a1458e034b6348a1
dericward <dericward@hotmail.com>
2023-01-16 16:21:04 +0000

    Now fully working and outputting to the webpage, forced the program to quit after generating the password, and added more comments to the code

--- assets/script.js
+++ assets/script.js
@@ -84,12 +84,12 @@ var upperCasedCharacters = [
     'Y',
     'Z'
 ];
-// initialise global variables and arrays
+// define and initialise global variables and arrays
 let randomArrayIndex = 0;
 let validArrays = [];
 let validArraysFlat = [];
 let passwordCharacter = "";
-let generatedPassword = "";
+let password = "";
 
 // OBJECT containing user-selected password options
 let passwordOptions = {
@@ -98,7 +98,7 @@ let passwordOptions = {
     charsLower: false,
     nums: false,
     specials: false
-};
+};//end OBJECT
 
 // Function to prompt user for password options and store the results in the object: passwordOptions
 function getPasswordOptions() {
@@ -107,6 +107,7 @@ passwordOptions.passwordLength = prompt("Please choose the length of your requir
 }
 while ((passwordOptions.passwordLength < 10) || (passwordOptions.passwordLength > 64)); // error check the user's input
 // end of waiting for user to enter a number in the corect range
+
 //start of waiting for user to choose at least one dataset to build the password from
 do {
 alert("You will now be presented with series of four password options. \nYou must choose 'OK' for at least one of the options presented.\nPress 'OK' to start:");
@@ -115,63 +116,64 @@ passwordOptions.charsLower = confirm("Do you want to use lowercase characters in
 passwordOptions.nums = confirm("Do you want to use numbers in your password? \nPress OK for yes, Cancel for no:");
 passwordOptions.specials = confirm("Do you want to use special characters in your password? \nPress OK for yes, Cancel for no:");
 }// ensure at least one of the datasets/types is selected, uses BOOLEAN LOGIC with AND (&&) and NOT (!) operators. 
-// These variables are initialised as boolean, '!value' is the same as 'value === false' as 'value' means 'value === true'.
+// These variables are initialised as boolean, '!value' (NOT 'value') is the same as 'value === false' (as 'value' means 'value === true').
 while ((!passwordOptions.charsUpper) && (!passwordOptions.charsLower) && (!passwordOptions.nums) && (!passwordOptions.specials));
 //end of waiting for user to choose at least one dataset to build the password from
 };
 
 // build an array of user-selected character sets from the object: passwordOptions
 function buildPasswordArray() {
-if (passwordOptions.charsUpper) {
+if (passwordOptions.charsUpper) { // if charsUpper === true eg: the user has selected uppercased characters
     validArrays.push(upperCasedCharacters);
 };
-if (passwordOptions.charsLower) {
+if (passwordOptions.charsLower) { // if charsLower === true (see above)
     validArrays.push(lowerCasedCharacters);
 };
-if (passwordOptions.nums) {
+if (passwordOptions.nums) { // see above
     validArrays.push(numericCharacters);
 };
-if (passwordOptions.specials) {
+if (passwordOptions.specials) { // see above
     validArrays.push(specialCharacters);
 };
 // flatten the nested arrays to make random element selection easier
 validArraysFlat = validArrays.flat();
 };// end of build new array and build another array that is a FLATened version
 
-// Utility Function random number generator
-// *** PLEASE NOTE: I COPIED THIS FUNCTION 'getRandomIntInclusive' directly from Mozilla's MDN WEB DOCS ***  
-// at: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};// end of COPIED function
-
 // construct the password by referencing the passwordOptions OBJECT, use the getRandomIntInclusive function to 
 // randomly select from the FLATtened array of user-selected character types
-function buildPassword() {
-for (let i = 0; i < passwordOptions.passwordLength; i++) {
-randomArrayIndex = getRandomIntInclusive(0, (validArraysFlat.length -1));
-passwordCharacter = validArraysFlat[randomArrayIndex];
-generatedPassword = generatedPassword + passwordCharacter;
-};
-return generatedPassword;
+function generatePassword() {
+    //NESTED FUNCTION to generate a random integer
+    function randomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };//end of nested function
+    // main function, uses the nested function (randomInt) above
+    for (let i = 0; i < passwordOptions.passwordLength; i++) {
+    randomArrayIndex = randomInt(0, (validArraysFlat.length -1));
+    passwordCharacter = validArraysFlat[randomArrayIndex];
+    password = password + passwordCharacter;
+    };
+    return password;
 };//end of password generation
 
-getPasswordOptions();
-buildPasswordArray();
-buildPassword();
+getPasswordOptions(); // run the function that asks the user to input their choices
+buildPasswordArray(); // build the dataset 0f arrays the use has chosen
+generatePassword();  // generate a random password from the data set. 
+//I've globally-scoped the generatePassword call so that it 'could' be used elsewhere easily
+//instead of it residing in the writePassword function.
 
-console.log(`Upper case characters? : ${passwordOptions.charsUpper}`);
-console.log(`Lower case characters? : ${passwordOptions.charsLower}`);
-console.log(`Numbers? : ${passwordOptions.nums}`);
-console.log(`Special characters? : ${passwordOptions.specials}`);
-
-console.log(`valid arrays : ${validArrays}`);
-console.log(`Length (number) of nested arrays in ValidArray: ${validArrays.length}`);
-console.log(`Valid arrays FLATened : ${validArraysFlat}`);
-console.log(`Flat Array length : ${validArraysFlat.length}`);
-
-console.log(`Password length : ${passwordOptions.passwordLength}`);
-console.log(`Last randomArrayIndex: ${randomArrayIndex}`);
-console.log(`Generated password: ${generatedPassword}`);
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+// Write password to the #password input
+function writePassword() {
+    var passwordText = document.querySelector('#password');
+    passwordText.value = password;
+    return // This return ensures the program only outputs the password once -
+    // without this return statement, the program keeps generating passwords 
+    // every time the button is clicked, each time the newly generated password
+    // being appended to the already generated (or appeneded to) password 
+    // (until some buffer is full, perhaps)
+};
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
